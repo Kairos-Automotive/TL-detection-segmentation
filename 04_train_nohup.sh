0d166e1c51d0f2edb0d44755de4d1604c4c431aa
Alexey <alexey.simonov@gmail.com>
2017-10-09 14:33:42 +0100

    tweaking default training parameters

--- 04_train_nohup.sh
+++ 04_train_nohup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup python main.py train --gpu=1 --xla=2 -ep=10 -bs=10 -lr=0.0001 > train-nohup.out 2>&1 &
+nohup python main.py train --gpu=1 --xla=2 -ep=4 -bs=10 -lr=0.0005 > train-nohup.out 2>&1 &
